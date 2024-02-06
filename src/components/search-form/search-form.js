606e70ca65c71a4d1eb3dbc6c0d0a167064bb945
omar-home-dekstop <medzhidov.omar11@gmail.com>
2024-02-06 21:55:10 +0300

    не нужное

--- src/components/search-form/search-form.js
+++ src/components/search-form/search-form.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import { debounce } from 'lodash'
 
-import SearchEngine from '../../services/searchEngine'
 import './search-form.css'
 
 export default class SearchForm extends React.Component {
-  searchEngine = new SearchEngine()
-
   render() {
     return (
       <form
